f7594b7c8498f92e748099164f286ffeb64a00e5
user.name <000010964109634sp@al.educacao.sp.gov.br>
2024-06-05 18:28:49 -0300

    FIM

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 def menu_principal():
     print("/n**Calculadora de Conversão de Unidades**")
-    print("1. Conveter Comprimento")
-    print("3. Sair")
+    print("1. Conveter de Comprimento")
+    print("2. converter de temperatura")
+    print("3.sair ")
 
     opcao = input("Digite a opção desejada: ")
     return opcao
 
+
 def sair():
     print('Encerrando o programado...')
 
-    escolha = menu_principal()
+escolha = menu_principal()
 
-    if escolha == '3':
+if escolha == '3':
         sair()
+elif escolha == '2':
+      from conversor_temperaturas import ConvertorTemperaturas
+elif escolha == '1' : 
+        from conversor_medidas import ConvertorMedidas 
