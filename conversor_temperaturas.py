79d4ce7a1725d26cc3607e68d48d005a939bc8a1
user.name <000010964109634sp@al.educacao.sp.gov.br>
2024-06-05 18:02:42 -0300

    em fim terminamos

--- conversor_temperaturas.py
+++ conversor_temperaturas.py
@@ -1,3 +1,19 @@
 class ConvertorTemperaturas:
     def __init__(self):
         pass
+
+    def celsiusParaFahrenheit(celsius):
+        Fahrenheit = (9/5*celsius)-32
+        return Fahrenheit
+
+    valor_em_celsius = 32
+    valor_em_Fahrenheit = celsiusParaFahrenheit(valor_em_celsius)
+    print(f"{valor_em_celsius} celsius equivalem a {valor_em_Fahrenheit:.2f} Fahrenheit.")
+
+    def celsiusParaFahrenheit(Fahrenheit):
+        celsius = 5/9 *(Fahrenheit - 32)
+        return celsius
+
+    valor_em_Fahrenheit = 57
+    valor_em_celsius = celsiusParaFahrenheit(valor_em_Fahrenheit)
+    print(f"{valor_em_Fahrenheit} Fahrenheit equivalem a {valor_em_celsius:.2f} celsius.")
